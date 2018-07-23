137cef24f38799e484c4083fe722c0abe88e0afc
Melina <sayeghmelina@gmail.com>
2018-07-23 05:53:31 -0400

    added lesson service

---
+++ src/services/LessonService.js
@@ -0,0 +1,71 @@
+let _singleton = Symbol();
+
+const C_M_L_API_URL = 'http://localhost:8080/api/course/CID/module/MID';
+const LESSON_API_URL = 'http://localhost:8080/api/lesson';
+
+
+export default class LessonService {
+    constructor(singletonToken) {
+        if (_singleton !== singletonToken)
+            throw new Error('Singleton!!!');
+    }
+
+    static get instance() {
+        if(!this[_singleton])
+            this[_singleton] = new LessonService(_singleton);
+        return this[_singleton]
+    }
+
+    findLessonById(lessonId) {
+        return fetch( LESSON_API_URL + '/' +  lessonId, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json();
+        })
+    }
+
+    findAllLessons() {
+        return fetch(LESSON_API_URL, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json();
+        })
+    }
+
+    findAllLessonsForModule(courseId, moduleId) {
+        return fetch(C_M_L_API_URL.replace('CID', courseId).replace('MID', moduleId), {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json();
+        })
+    }
+
+    createLesson(courseId, moduleId) {
+        return fetch(C_M_L_API_URL.replace('CID', courseId).replace('MID', moduleId), {
+            method: 'POST',
+            body: JSON.stringify(module),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json(); })
+    }
+
+    deleteLesson(lessonId) {
+        return fetch(LESSON_API_URL + '/' + moduleId, {
+            method: 'DELETE',
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json(); })
+    }
+
+    updateLesson(lesson, lessonId) {
+        return fetch(LESSON_API_URL + '/' + lessonId, {
+            method: 'PUT',
+            body: JSON.stringify(module),
+            headers: { 'Content-Type': 'application/json' }
+        }).then(function (response) {
+            return response.json(); })
+    }
+}

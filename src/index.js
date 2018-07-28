c6350663561009310ed69de13e3413f4ad33a4e9
Melina <sayeghmelina@gmail.com>
2018-07-28 12:32:12 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import CourseManager from './containers/CourseManager.js';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import '../node_modules/font-awesome/css/font-awesome.min.css';
 
-
-
 ReactDOM.render(
     <div className="container-fluid">
         <CourseManager/>
     </div>,
     document.getElementById('root')
 );
+
+
+
